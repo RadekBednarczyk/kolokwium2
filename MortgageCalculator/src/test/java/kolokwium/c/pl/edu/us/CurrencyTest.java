c52b3df05abdde5dee3c7576ecde666dc9ec08e1
elPakejro <rradoslawbednarczyk@gmail.com>
2016-05-30 19:13:24 +0200

    Punkt 5 i 6

--- MortgageCalculator/src/test/java/kolokwium/c/pl/edu/us/CurrencyTest.java
+++ MortgageCalculator/src/test/java/kolokwium/c/pl/edu/us/CurrencyTest.java
@@ -4,15 +4,32 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-public class CurrencyTest
-{
+public class CurrencyTest {
 	@Test
 	public void convertFromPlnTest() {
-		fail("Not yet implemented");
+
+		Currency c = new Currency("Frank", 2.0);
+		boolean result = false;
+
+		if (c.convertFromPLN(34) == 17.0) {
+			result = true;
+		}
+
+		assertTrue("Blad w konwersji z pln", result);
+
 	}
-	
+
 	@Test
 	public void convertToPlnTest() {
-		fail("Not yet implemented");
+		Currency c = new Currency("Frank", 4.0);
+		boolean result = false;
+
+		
+		if (c.convertToPLN(20) == 80.0) {
+			result = true;
+		}
+
+		assertTrue("Blad w konwersji na pln", result);
+
 	}
 }
